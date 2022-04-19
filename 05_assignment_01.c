17083f26508b4392191a48e1806127e4e3935502
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2022-04-19 05:18:24 +0000

    added a new file 05_assignent_01.c

---
+++ 05_assignment_01.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+# define SIZE 10
+int main(void) {
+  int a[SIZE] = {1,2,3,5,6,7,8,9};
+  for (int i=0; i<SIZE; i++) 
+    printf("%d \t",a[i] );
+  a[10] = 25;
+  printf("\nafter inserting...\n");
+  for (int i=0; i<SIZE+1; i++) 
+    printf("%d \t",a[i] );
+}
+
+// Static allocation of array will lead to run time errors while inserting new elements.  Alternatively we must use dynamic memory allocation - malloc()
